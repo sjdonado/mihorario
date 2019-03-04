afd8da8f27809ac96abdcb6ac258505ff0b489f1
WilliamCadenas21 <williamcadenas21@gmail.com>
2019-03-04 00:07:12 -0500

    fix: synchronize the complete schedule

--- app/constrollers/GoogleCalendarController.js
+++ app/constrollers/GoogleCalendarController.js
@@ -23,75 +23,84 @@ function holidaysByDate(date) {
   );
 }
 
+async function createEvent(newEvent, calendarService){
+  try {
+    const response = await calendarService.createEvent(newEvent)
+    if(response.status !== 200){
+      await createEvent(newEvent,calendarService);
+    }
+  } catch (e) {
+    console.log(e);
+  }
+}
+
 /**
  * [ALL] Add the events to user calendar
  */
 GoogleCalendarRouter.all("/create", async (req, res) => {
-  const { subject, tokens, notif_time } = req.body;
+  try {
+    const { subject, tokens, notif_time } = req.body;
 
-  if (!subject) return res.redirect("/subjects");
-
-  let subjects = subject;
-
-  if (subjects.constructor !== Array) subjects = [subjects];
-
-  subjects = subjects.map(el => {
-    return JSON.parse(el);
-  });
-
-  const calendarService = new CalendarService(tokens);
-
-  subjects.forEach(subject => {
-    subject.meetingPatterns.forEach(ev => {
-      const newEvent = {
-        location: ev.building + ", " + ev.room,
-        summary: subject.sectionTitle,
-        description: subject.instructors[0].formattedName,
-        start: {
-          dateTime: moment(
-            `${ev.startDate} ${ev.sisStartTimeWTz.substr(0, 5)} GMT-0500`
-          )
-            .add(ev.daysOfWeek - 2, "days")
-            .format(),
-          timeZone: "America/Bogota"
-        },
-        end: {
-          dateTime: moment(
-            `${ev.startDate} ${ev.sisEndTimeWTz.substr(0, 5)} GMT-0500`
-          )
-            .add(ev.daysOfWeek - 2, "days")
-            .format(),
-          timeZone: "America/Bogota"
-        },
-        reminders: {
-          useDefault: false,
-          overrides: [
-            {
-              method: "popup",
-              minutes: notif_time || 20
-            }
-          ]
-        },
-        recurrence: [
-          `RRULE:FREQ=WEEKLY;UNTIL=${subject.lastMeetingDate
-            .split("-")
-            .join("")}`,
-          ...holidaysByDate(ev.sisStartTimeWTz.substr(0, 5))
-        ]
-      };
-      calendarService
-        .createEvent(newEvent)
-        .then(res => console.log('RES', res))
-        .catch(e => {
-          console.log(e);
-          reportError(e, newEvent);
-        });
+    if (!subject) return res.redirect("/subjects");
+  
+    let subjects = subject;
+  
+    if (subjects.constructor !== Array) subjects = [subjects];
+  
+    subjects = subjects.map(el => {
+      return JSON.parse(el);
     });
-  });
-
-  req.session.done = true;
-
-  return res.redirect("/done");
+  
+    const calendarService = new CalendarService(tokens);
+  
+    subjects.forEach(async subject => {
+      subject.meetingPatterns.forEach(async ev => {
+        const newEvent = {
+          location: ev.building + ", " + ev.room,
+          summary: subject.sectionTitle,
+          description: subject.instructors[0].formattedName,
+          start: {
+            dateTime: moment(
+              `${ev.startDate} ${ev.sisStartTimeWTz.substr(0, 5)} GMT-0500`
+            )
+              .add(ev.daysOfWeek - 2, "days")
+              .format(),
+            timeZone: "America/Bogota"
+          },
+          end: {
+            dateTime: moment(
+              `${ev.startDate} ${ev.sisEndTimeWTz.substr(0, 5)} GMT-0500`
+            )
+              .add(ev.daysOfWeek - 2, "days")
+              .format(),
+            timeZone: "America/Bogota"
+          },
+          reminders: {
+            useDefault: false,
+            overrides: [
+              {
+                method: "popup",
+                minutes: notif_time || 20
+              }
+            ]
+          },
+          recurrence: [
+            `RRULE:FREQ=WEEKLY;UNTIL=${subject.lastMeetingDate
+              .split("-")
+              .join("")}`,
+            ...holidaysByDate(ev.sisStartTimeWTz.substr(0, 5))
+          ]
+        };
+        await createEvent(newEvent, calendarService);
+      });
+    });
+  
+    req.session.done = true;
+  
+    return res.redirect("/done");
+  } catch (e) {
+    console.log(e);
+  }
 });
 
 module.exports = GoogleCalendarRouter;
