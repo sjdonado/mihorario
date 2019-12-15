88cda2d5f6dee9e1131c0c3389acab1165e06b6a
Juan Rodríguez <jsrd98@gmail.com>
2019-12-14 23:15:43 -0500

    fix: Hostname env variable removed

--- server/index.js
+++ server/index.js
@@ -5,11 +5,10 @@ const config = require('./src/config/');
 
 const {
   port,
-  hostname,
 } = config.server;
 
 const server = http.createServer(app);
 
-server.listen(port, hostname, () => {
-  console.log(`Server http://${hostname}:${port} \x1b[32m%s\x1b[0m`, 'online');
+server.listen(port, (hostname) => {
+  console.log('\x1b[32m%s\x1b[0m', `Server listening on ${port}`);
 });
--- server/src/config/index.js
+++ server/src/config/index.js
@@ -1,7 +1,6 @@
 const config = {
   server: {
     port: process.env.PORT,
-    hostname: process.env.HOSTNAME,
     clientName: process.env.CLIENT_NAME,
     secret: process.env.SECRET,
     uninorteGraphQL: process.env.UNINORTE_GRAPHQL,
